bd1577c09ec60f51aae282fed400346bbde02c18
krnfrmh <87569650+krnfrmh@users.noreply.github.com>
2021-10-20 22:48:33 +0530

    Initiate Fit method

--- pseudo_random_forest.py
+++ pseudo_random_forest.py
@@ -7,3 +7,12 @@ class PseudoRandomForest:
   
   def __init__(self, n_estimators):
     self.B = n_estimators
+
+  def fit(self, X, Y, M=None):
+    N, D = X.shape
+    if M is None:
+      # Number of hidden units as per RandomForest creator
+      M = int(np.sqrt(D))
+
+    self.models = []
+    self.features = []
