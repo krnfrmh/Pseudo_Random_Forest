e3bae2fd06c820f7929795b64e4187fe51c41633
krnfrmh <87569650+krnfrmh@users.noreply.github.com>
2021-10-28 23:44:16 +0530

    Generating Bootstrapped samples with replacement

--- bagged_tree.py
+++ bagged_tree.py
@@ -6,3 +6,10 @@ class BaggedTree:
   def __init__(self, B):
     self.B = B
     
+  def fit(self, X, Y):
+    N = len(X)
+    self.models = []
+    for b in range(self.B):
+      idx = np.random.choice(N, size=N, replace=True)
+      Xb = X[idx]
+      Yb = Y[idx]
